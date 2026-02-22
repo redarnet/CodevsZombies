9b91d3cda0d6d98cb47f37a87869cd262950b2d2
Remi DARNET <redarnet@f4r2s6.clusters.42paris.fr>
2026-02-22 21:40:51 +0100

    fall algo start backtrack

--- thefall/thefall.cpp
+++ thefall/thefall.cpp
@@ -23,31 +23,33 @@ class Position{
 bool verif_pos(int i, string c)
 {
     if (i == 0)
+    {
         return false;
+    }
     if (c == "TOP")
     {
-        if (i == 1 || i == 3 || i == 4 || i == 5 || 7 || i == 9 || i == 10 || i == 11)
+        if (i == 1 || i == 3 || i == 4 || i == 5 || i == 7 || i == 9 || i == 10 || i == 11)
             return true;
     }
     else if (c =="RIGHT")
     {
-        if (i == 1 ||i == 4 || i == 2 || i == 8 || i == 12 || i == 7)
+        if (i == 1 ||i == 4 ||i == 5 || i == 2 || i == 8 || i == 12 || i == 7)
             return true;
     }
     else if (c == "LEFT")
     {
-        if (i == 1 ||i == 5 || i == 2 || i == 8 || i == 13 || i == 9)
+        if (i == 1 ||i == 5 || i == 4 || i == 2 || i == 8 || i == 13 || i == 9)
             return true;
     }
     return false;
 }
 
 
-
-Position next_pos(char c, int yi, int xi, string posi)
+Position next_pos(int c, int yi, int xi, string posi)
 {
         int xp = xi;
         int yp = yi;
+        c = abs(c);
         switch(c){
             case 2:
                 if(posi=="LEFT")
@@ -84,84 +86,82 @@ Position next_pos(char c, int yi, int xi, string posi)
                 break;  
         }
         string posp;
-        if (yi ==  yp - 1)
-            posp = "BOT";
-        else if (yi == yp +1)
-            posp = "TOP";
-        else if (xi ==  xp - 1)
-            posp = "LEFT";
-        else if (xi == xp + 1)
-            posp = "RIGHT";
+        if (yi == yp + 1)
+               posp = "TOP";  
+        else if (yi == yp - 1)
+          posp = "BOT";   
+        else if (xi == xp + 1)  
+            posp = "LEFT";  
+        else if (xi == xp - 1)  
+            posp = "RIGHT"; 
     // next_pos == pos + posp 
     Position p(xi, yi, posp);
     return p;
     
 }
+
 vector<string> sol;
 
-vector<string> simu(vector<vector<int>> grille, int yi, int xi, string pos)
+
+bool isValid(vector<vector<int>> grille, int yi, int xi, string pos)
 {
+    if (yi == grille.size())
+        return true;
+    if (xi < 0 || xi >= grille[0].size() || yi < 0)
+        return false;
 
-    Position p(xi, yi, pos);
-    cout << "POS = " << p.x << endl;
-    cout << "POS = " << p.y << endl;
-    int x =  xi;
-    int y = yi;
-    bool verif = true;
-    while ( p.y  != grille.size())
+    int cell = grille[yi][xi];
+    bool locked = (cell < 0);
+    cell = abs(cell);  // travaille toujours avec la valeur absolue
+    if (cell == 0)
+        return false;
+
+    if (!verif_pos(cell, pos))
     {
-        cout << " POS =" <<  p.y << " " << p.x << endl;
-        cout << " POS =" <<  grille[p.y][p.x] << endl;
-        x =  p.x;
-        y = p.y;
-        p = next_pos(grille[p.y][p.x], p.y, p.x, pos); 
-        if ( p.y != grille.size() && !verif_pos(grille[p.y][p.x], p.pos))
+        if (!locked)
         {
-            cout << " ERROR" << endl;
-            verif = false;
-            break;
-            // return false;
+            if (cell == 13 && verif_pos(10, pos))
+            {
+                cerr << " Par ici " << endl;
+                grille[yi][xi] = 10;
+                sol.push_back(to_string(xi) + " " + to_string(yi) + " RIGHT");
+                Position p = next_pos(grille[yi][xi], yi, xi, pos);
+                if (isValid(grille, p.y, p.x, p.pos))
+                    return true;
+                cerr << " Par ici " << endl;
+                sol.pop_back();
+                grille[yi][xi] = cell; 
+
+            }
+            if (cell < 13 && verif_pos(cell + 1, pos))
+            {
+                grille[yi][xi] = cell + 1;
+                sol.push_back(to_string(xi) + " " + to_string(yi) + " RIGHT");
+                Position p = next_pos(grille[yi][xi], yi, xi, pos);
+                if (isValid(grille, p.y, p.x, p.pos))
+                    return true;
+                sol.pop_back();
+                grille[yi][xi] = cell; 
+            }
+            if (cell > 1 && verif_pos(cell - 1, pos))
+            {
+                grille[yi][xi] = cell - 1;
+                sol.push_back(to_string(xi) + " " + to_string(yi) + " LEFT");
+                Position p = next_pos(grille[yi][xi], yi, xi, pos);
+                if (isValid(grille, p.y, p.x, p.pos))
+                    return true;
+                sol.pop_back();
+                grille[yi][xi] = cell; 
+            }
         }
+        if (grille[yi][xi] > 0)
+            grille[yi][xi] = -grille[yi][xi]; 
+        return false;
     }
-    if (!verif)
-    {
-        // verif if grille valide > si oui ok si nn test - 1  sinon test + 2 etc 
-        if (grille[y][x] <= 12)
-            grille[y][x] += 1;
-        else if (grille[y][x] == 13)
-            grille[y][x] = 1;
-        cout << " One  loop" << endl;
-        // cout << " POS =" <<  y << " " << p.x << endl;
-        // cout << " GRILLE[]" << grille[y][x] <<  endl;
-        string s = to_string(x) + " " + to_string(y) + " RIGHT";  
-        sol.push_back(s);
-        simu(grille, yi, xi, pos);
-    }
-    //     relance simu avec backtracing et piece fausse + 1;nb par piece == pos ? 
-    // if (simu == true good)
-    //     get all change  
-    return sol;
 
+    Position p = next_pos(cell, yi, xi, pos);
+    return isValid(grille, p.y, p.x, p.pos);
 }
-
-
-// 0 0 -3 0 0
-// 0 0 2 0 0
-// 0 0 -3 0 0
-// 0 0 -27 0 0 
-// 0 0 2 0 0 
-// 0 0 -27 0 0 
-
-
-// 0 0 0 0 0 3 
-// 8 3 3 2 2 10 
-// 2 0 0 0 10 13 
-// 11 3 2 3 1 13 
-// 3 10 0 0 2 0 
-// 0 6 3 3 4 13 
-// 0 3 0 13 4 10 
-// 0 13 2 4 10 0 
-// 0 0 0 3 0 0 
 
 int main()
 {
@@ -169,15 +169,15 @@ int main()
     // int h; // number of rows.
     // cin >> w >> h; cin.ignore();
     vector<vector<int>> grille;
-    vector<int> v1= {0,0,0,0,0,3};
-    vector<int> v2= {8,3,3,2,2,10};
-    vector<int> v3= {2,0,0,0,10,13};
-    vector<int> v4= {11,3,2,3,1,13};
-    vector<int> v5= {3,10,0,0,2,0};
-    vector<int> v6= {0,6,3,3,4,13};
-    vector<int> v7= {0,3,0,1 ,3,4,10};
-    vector<int> v8= {0,13,2,4, 10,0};
-    vector<int> v9= {0,0,0,3, 0,0};
+    vector<int> v1= {0, -3, 0,  0,  0,  0,  0,  0};
+    vector<int> v2= {0, 12, 3,  3,  2,  3,  12, 0};
+    vector<int> v3= {0, 0,  0,  0,  0,  0,  2,  0};
+    vector<int> v4= {0, -12, 3,  2,  2,  3,  13, 0};
+    vector<int> v5= {0, 3,  0,  0,  0,  0,  0,  0};
+    vector<int> v6= {0, 12, 3,  3,  2,  3,  12, 0};
+    vector<int> v7= {0, 0,  0,  0,  0,  0,  2,  0};
+    vector<int> v8= {0, 12, 3,  2,  2,  3,  13, 0};
+     
     grille.push_back(v1);
     grille.push_back(v2);
     grille.push_back(v3);
@@ -186,7 +186,7 @@ int main()
     grille.push_back(v6);
     grille.push_back(v7);
     grille.push_back(v8);
-    grille.push_back(v9);
+
     for (int i = 0; i < grille.size(); i++)
     {
         for (int j = 0; j < grille[0].size(); j++)
@@ -200,8 +200,8 @@ int main()
     ex = 2;
     // cin >> ex; cin.ignore();
     cerr << " Ex = " << ex << endl;
-    vector<string> sol;
-    sol = simu(grille, 0, 5, "TOP");
+    // vector<string> sol;
+    isValid(grille, 0, 1, "TOP");
     for (auto c : sol)
     {
         cout << c << endl;
